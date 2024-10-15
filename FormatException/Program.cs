f155581416c1470336970559f0300ba11d35eda5
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 18:44:21 +0530

    Add project files.

---
+++ FormatException/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormatException
+{
+    class BankAccount
+    {
+        public string AccountHolderName { get; set; }
+        public int AccountNumber { get; set; }
+        public double CurrentBalance { get; set; }
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Console.Write("enter account holder name:");
+            account.AccountHolderName=Console.ReadLine();
+            Console.Write("ENter account number:");
+            account.AccountNumber=int.Parse(Console.ReadLine());
+            Console.Write("Enter current balance:");
+            account.CurrentBalance=double.Parse(Console.ReadLine());
+            Console.WriteLine("\n New bank account details");
+            Console.WriteLine("Account holder name:"+account.AccountHolderName);
+            Console.WriteLine("Account number:"+account.AccountNumber);
+            Console.WriteLine("Current Balance:"+account.CurrentBalance);
+
+            Console.ReadKey();
+        }
+    }
+}
