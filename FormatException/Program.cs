9c4fa78d2d2fcc819a33d9cce8c787b5efc628a1
janani-sankar-1 <sec21it039@sairamtap.edu.in>
2024-10-15 18:49:36 +0530

    Update Program.cs

--- FormatException/Program.cs
+++ FormatException/Program.cs
@@ -1,32 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace FormatException
-{
+
     class BankAccount
     {
         public string AccountHolderName { get; set; }
         public int AccountNumber { get; set; }
         public double CurrentBalance { get; set; }
     }
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            BankAccount account = new BankAccount();
+            try
+            {
+                BankAccount account = new BankAccount();
 
-            Console.Write("enter account holder name:");
-            account.AccountHolderName=Console.ReadLine();
-            Console.Write("ENter account number:");
-            account.AccountNumber=int.Parse(Console.ReadLine());
-            Console.Write("Enter current balance:");
-            account.CurrentBalance=double.Parse(Console.ReadLine());
-            Console.WriteLine("\n New bank account details");
-            Console.WriteLine("Account holder name:"+account.AccountHolderName);
-            Console.WriteLine("Account number:"+account.AccountNumber);
-            Console.WriteLine("Current Balance:"+account.CurrentBalance);
+                Console.Write("enter account holder name:");
+                account.AccountHolderName = Console.ReadLine();
+                Console.Write("ENter account number:");
+                account.AccountNumber = int.Parse(Console.ReadLine());
+                Console.Write("Enter current balance:");
+                account.CurrentBalance = double.Parse(Console.ReadLine());
+                Console.WriteLine("\n New bank account details");
+                Console.WriteLine("Account holder name:" + account.AccountHolderName);
+                Console.WriteLine("Account number:" + account.AccountNumber);
+                Console.WriteLine("Current Balance:" + account.CurrentBalance);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadKey();
         }
     }
-}
+
